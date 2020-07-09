0cae134febd08db5f315e2d05b6e1104ec348b66
Newton <kamaunewton78@gmail.com>
2020-07-09 07:48:53 +0300

    adding success or failure pages

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ app.post("/", function (req, res) {
     auth: "Newton:474270166f07fdf0d06106bb6aa77abf-us10",
   };
   const request = https.request(url, options, function (response) {
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html")
+    }
     response.on("data", function (data) {
       console.log(JSON.parse(data));
     });
