fcb02ac34ea7db9a858d66ddb564cbb8f2b5b32a
Newton <kamaunewton78@gmail.com>
2020-07-09 06:02:32 +0300

    created the post method for our form and logged the values from the form

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 //jshint esversion : 6
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require('request');
+const request = require("request");
+const { response } = require("express");
 const app = express();
 
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/signup.html");
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function (req, res) {
+  var firstName = req.body.firstname;
+  var lastName = req.body.lastname;
+  var email = req.body.email;
+  console.log(firstName + lastName + email);
 });
 
 app.listen(3000, function () {
-    console.log("serving at 3000");
-    
+  console.log("serving at 3000");
 });
-
-
-
