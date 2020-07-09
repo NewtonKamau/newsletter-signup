db73b5b1819522fffe418887173054e9b0cab025
Newton <kamaunewton78@gmail.com>
2020-07-09 12:57:15 +0300

    put comma

--- app.js
+++ app.js
@@ -35,13 +35,13 @@ app.post("/", function (req, res) {
   //options to help with authentication
   const options = {
     method: "POST",
-    auth: "Newton:{ApiKey}",
+    auth: "Newton:474270166f07fdf0d06106bb6aa77abf-us10",
   };
   const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html");
     } else {
-      res.sendFile(__dirname + "/failure.html")
+      res.sendFile(__dirname + "/failure.html");
     }
     response.on("data", function (data) {
       console.log(JSON.parse(data));
@@ -56,9 +56,8 @@ app.post("/failure", function (req,res) {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("serving at 3000");
 });
 
-//api key
-//
+
