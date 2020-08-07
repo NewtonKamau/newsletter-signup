83b49db80706fc53c19777fad5207160459311c3
Newton <kamaunewton78@gmail.com>
2020-08-07 10:57:23 +0300

    resolve

--- app.js
+++ app.js
@@ -35,13 +35,13 @@ app.post("/", function (req, res) {
   //options to help with authentication
   const options = {
     method: "POST",
-    auth: "{}",
+    auth: "{APIKEY}",
   };
   const request = https.request(url, options, function (response) {
     if (response.statusCode === 200) {
       res.sendFile(__dirname + "/success.html");
     } else {
-      res.sendFile(__dirname + "/failure.html");
+      res.sendFile(__dirname + "/failure.html")
     }
     response.on("data", function (data) {
       console.log(JSON.parse(data));
@@ -56,8 +56,9 @@ app.post("/failure", function (req,res) {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(3000, function () {
   console.log("serving at 3000");
 });
 
-
+//api key
+//
